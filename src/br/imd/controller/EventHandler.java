a288f9d90587e71de9c852ac0d44f59a9f0916a0
Eduardo Sarmento <edusrmt@ufrn.edu.br>
2019-11-10 21:24:17 -0300

    Precisão implementada

--- src/br/imd/controller/EventHandler.java
+++ src/br/imd/controller/EventHandler.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
@@ -19,13 +20,32 @@ import br.imd.vision.People;
 
 public class EventHandler {
 	@FXML
+	private TextField precisionField;
+	
+	@FXML
 	private Button loadButton;
 	
 	@FXML
 	private TextArea outputTextArea;
 	
+	private int timeStep = 5;
+	
 	@FXML
 	private void initialize() {
+		precisionField.textProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue.matches("\\d*"))
+				precisionField.setText(newValue.replaceAll("[^\\d]", ""));	
+		});
+		
+		precisionField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+			if (oldValue) {
+				if (precisionField.getText().isEmpty())
+					precisionField.setText("5");
+				
+				timeStep = Integer.parseInt(precisionField.getText());
+			}
+		});
+		
 		loadButton.setOnAction(this::selectVideoFile);
 	}
 	
@@ -40,12 +60,12 @@ public class EventHandler {
 			String videoPath = selectedFile.getPath().replace("\\", "/");
 			
 			try {
-				Video inputVideo = VideoReader.read(videoPath, 5);
-				People.detect(inputVideo, 5, outputTextArea);
+				Video inputVideo = VideoReader.read(videoPath, timeStep);
+				People.detect(inputVideo, timeStep, outputTextArea);
 			} catch (UnicodeFilePathException e) {
 				Alert alert = new Alert(AlertType.ERROR, "Can't open a file with Unicode characters in its path!", ButtonType.CLOSE);
 				alert.showAndWait();
 			}
 		}
-	}
+	}	
 }
